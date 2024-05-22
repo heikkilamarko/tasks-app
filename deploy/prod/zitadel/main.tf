36a7e3601905389c38a0be6631554eecf30a7b52
Marko Heikkilä <heikkila_marko@hotmail.com>
2024-05-22 16:05:26 +0300

    Update deps.

--- deploy/prod/zitadel/main.tf
+++ deploy/prod/zitadel/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     zitadel = {
       source  = "zitadel/zitadel"
-      version = "1.1.1"
+      version = "1.2.0"
     }
   }
 }
@@ -12,6 +12,23 @@ provider "zitadel" {
   insecure         = false
   port             = 443
   jwt_profile_file = "./machinekey/zitadel-admin-sa.json"
+}
+
+# Instance
+
+resource "zitadel_default_label_policy" "zitadel" {
+  primary_color          = "#5469d4"
+  hide_login_name_suffix = true
+  warn_color             = "#cd3d56"
+  background_color       = "#fafafa"
+  font_color             = "#000000"
+  primary_color_dark     = "#a5b4fc"
+  background_color_dark  = "#111827"
+  warn_color_dark        = "#ff3b5b"
+  font_color_dark        = "#ffffff"
+  disable_watermark      = true
+  set_active             = true
+  theme_mode             = "THEME_MODE_DARK"
 }
 
 # Organizations
